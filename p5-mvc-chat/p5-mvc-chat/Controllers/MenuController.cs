fc3ed5491665ecb6fe95b21c17e245c462b98451
alucardlockon <alucardlockon3@gmail.com>
2017-08-05 16:20:40 +0800

    add new

---
+++ p5-mvc-chat/p5-mvc-chat/Controllers/MenuController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace p5_mvc_chat.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return
+            View();
+        }
+    }
+}    
